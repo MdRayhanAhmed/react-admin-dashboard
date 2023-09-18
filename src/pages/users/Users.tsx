2ab5c2f229df5be6594451e8f141d75f800ad6e7
Md Rayhan Ahmed <rayhanahmed641@gmail.com>
2023-09-18 21:32:30 +0600

    Update Users.tsx

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,8 +1,86 @@
+import { GridColDef } from "@mui/x-data-grid";
+import DataTable from "../../components/dataTable/DataTable";
+import "./Users.scss";
+// import Add from "../../components/add/Add";
+import { userRows } from "../../data";
+// import { useQuery } from "@tanstack/react-query";
 
-import './users.scss' 
-const Users =() =>{
-    return (
-        <div className="users">Users</div>
-    )
-}
-export default Users
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "img",
+    headerName: "Avatar",
+    width: 100,
+    renderCell: (params) => {
+      return <img src={params.row.img || "/noavatar.png"} alt="" />;
+    },
+  },
+  {
+    field: "firstName",
+    type: "string",
+    headerName: "First name",
+    width: 150,
+  },
+  {
+    field: "lastName",
+    type: "string",
+    headerName: "Last name",
+    width: 150,
+  },
+  {
+    field: "email",
+    type: "string",
+    headerName: "Email",
+    width: 200,
+  },
+  {
+    field: "phone",
+    type: "string",
+    headerName: "Phone",
+    width: 150,
+  },
+  {
+    field: "createdAt",
+    headerName: "Created At",
+    width: 100,
+    type: "string",
+  },
+  {
+    field: "verified",
+    headerName: "Verified",
+    width: 70,
+    type: "boolean",
+  },
+];
+
+const Users = () => {
+  // TEST THE API
+
+  // const { isLoading, data } = useQuery({
+  //   queryKey: ["allusers"],
+  //   queryFn: () =>
+  //     fetch("http://localhost:8800/api/users").then(
+  //       (res) => res.json()
+  //     ),
+  // });
+
+  return (
+    <div className="users">
+      <div className="info">
+        <h1>Users</h1>
+        <button>Add New User</button>
+      </div>
+      <DataTable slug="users" columns={columns} rows={userRows} />
+      {/* TEST THE API */}
+
+      {/* {isLoading ? (
+        "Loading..."
+      ) : (
+        <DataTable slug="users" columns={columns} rows={data} />
+      )} */}
+      {/* {open && <Add slug="user" columns={columns} setOpen={setOpen} />} */}
+    </div>
+  );
+};
+
+export default Users;
